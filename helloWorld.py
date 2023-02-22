893941639643bc1d04aa961f8c1103b3fbe90abc
unknown <G00425631@atu.ie>
2023-02-22 14:46:26 +0000

    sayname

---
+++ helloWorld.py
@@ -0,0 +1,3 @@
+# helloWorld.py
+# This program just prints Hello World
+# Author: Dorina Agache
---
+++ sayname.py
@@ -0,0 +1,7 @@
+# say name
+# authour: Dorina Agache
+
+
+name= input("Enter your name:")
+print(f"Your name is {name}")
+print("Thank you for your time!\nGoodybye!")
