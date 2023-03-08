24ef1e257eb227d6a7f66ac6cba55011508ac0f6
unknown <G00425631@atu.ie>
2023-03-08 20:45:12 +0000

    Week03

---
+++ Week03/length.py
@@ -0,0 +1,8 @@
+# length.py
+#This program counts the legth of a word
+#Author: Dorina Agache
+
+
+inputString = input('Enter a string:')
+lenghtOfString = len(inputString)
+print(f'The length of {inputString} is {lenghtOfString }')
